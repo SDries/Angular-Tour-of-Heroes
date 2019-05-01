ff195e5f78fe299236cf3a88f3da3fdad8223736
Dries Sercu <driessercu@gmail.com>
2019-05-01 17:00:09 +0200

    Added "Routing"
    https://angular.io/tutorial/toh-pt5

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -10,21 +10,15 @@ import { Hero } from '../hero';
 export class HeroesComponent implements OnInit {
   heroes: Hero[];
 
-  selectedHero: Hero;
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
-
-
   constructor(private heroService: HeroService) { }
+
+  ngOnInit() {
+    this.getHeroes();
+  }
 
   getHeroes(): void {
     this.heroService.getHeroes()
       .subscribe(heroes => this.heroes = heroes);
-  }
-
-  ngOnInit() {
-    this.getHeroes();
   }
 
 }
